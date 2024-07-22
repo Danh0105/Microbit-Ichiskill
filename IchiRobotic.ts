cd452d4465aaeae33636455d4279390cc74d0fdb
Danh0105 <150211320+Danh0105@users.noreply.github.com>
2024-07-22 14:17:42 +0700

    Cập nhật IchiRobotic.ts

--- IchiRobotic.ts
+++ IchiRobotic.ts
@@ -77,10 +77,15 @@ namespace IchiRobotic {
         );
     }
  
-   //% color="#fc0303"
-         //% blockId=servoservosetangle block="set %servo angle to %degrees=protractorPicker °"
-    //% speed.min=-255 speed.max=255
-    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=4
+ //% weight=100 help=servos/set-angle
+        //% blockId=servoservosetangle block="set %servo angle to %degrees=protractorPicker °"
+        //% degrees.defl=90
+        //% servo.fieldEditor="gridpicker"
+        //% servo.fieldOptions.width=220
+        //% servo.fieldOptions.columns=2
+        //% blockGap=8
+        //% parts=microservo trackArgs=0
+        //% group="Positional"
     export function setAngle(degree: number){
         IchiLib.P0.setAngle(degree)
     }
