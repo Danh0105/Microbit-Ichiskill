da62a92835e52cc7529b7ff8e498d2a874d606f3
Danh0105 <150211320+Danh0105@users.noreply.github.com>
2024-07-22 15:39:50 +0700

    Update IchiLib.ts, IchiRobotic.ts, targetoverrides.ts

--- targetoverrides.ts
+++ targetoverrides.ts
@@ -1,8 +1,8 @@
 namespace servos {
     //% block="servo P0" fixedInstance whenUsed
-    export const P0 = new IchiLib.PinServo(pins.P0);
+    export const P0 = new IchiRobotic.PinServo(pins.P0);
     //% block="servo P1" fixedInstance whenUsed
-    export const P1 = new IchiLib.PinServo(pins.P1);
+    export const P1 = new IchiRobotic.PinServo(pins.P1);
     //% block="servo P2" fixedInstance whenUsed
-    export const P2 = new IchiLib.PinServo(pins.P2);
+    export const P2 = new IchiRobotic.PinServo(pins.P2);
 }
