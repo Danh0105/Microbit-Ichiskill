1bb8c85bd95d61625f5ffaecb418baf446951916
Danh0105 <150211320+Danh0105@users.noreply.github.com>
2024-07-22 16:11:22 +0700

    Update pxt.json, servo.ts, targetoverrides.ts

--- targetoverrides.ts
+++ targetoverrides.ts
@@ -1,8 +1,1 @@
-namespace servos {
-    //% block="servo P0" fixedInstance whenUsed
-    export const P0 = new IchiRobotic.PinServo(pins.P0);
-    //% block="servo P1" fixedInstance whenUsed
-    export const P1 = new IchiRobotic.PinServo(pins.P1);
-    //% block="servo P2" fixedInstance whenUsed
-    export const P2 = new IchiRobotic.PinServo(pins.P2);
-}
+// Add your code here
